7b860e44ee52c98642f0241d8806a95af7753d9e
LevBuslaev <l.buslaev@gmail.com>
2018-11-27 16:03:47 +0300

    Initial commit Comment1

---
+++ src/LevBuslaev.java
@@ -0,0 +1,9 @@
+/*
+ * Java. MyProgramm Lev A Buslaev
+ * @version dated Nov 22.2018
+ */
+public class LevBuslaev {
+    public static void main(String[] args) {
+        System.out.println("LevBuslaev");
+    }
+}
